5c2caac0b725ed88e5579a9761932339e2c6eb74
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-13 18:50:21 -0400

    Ejercicio #5 completado

---
+++ 3_Eventos_Delegados/Ejercicio5_Eventos/AlarmClock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Ejercicio5_Eventos
+{
+    public class AlarmClock
+    {
+        private DateTime tiempoAlarma;
+        private System.Timers.Timer temporizador;
+
+        public event EventHandler EncendidoDeAlarma;
+
+        public AlarmClock(DateTime tiempo)
+        {
+            tiempoAlarma = tiempo;
+            temporizador = new System.Timers.Timer(1000);
+            temporizador.Elapsed += ComprobarTiempo;
+        }
+        public void Inicio()
+        {
+            temporizador.Start();
+        }
+        public void fin()
+        {
+            temporizador.Stop();
+        }
+        private void ComprobarTiempo(object sender, ElapsedEventArgs e)
+        {
+            if (DateTime.Now > tiempoAlarma)
+            {
+                temporizador.Stop();
+                EncendidoDeAlarma?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
--- 3_Eventos_Delegados/Ejercicio5_Eventos/Program.cs
+++ 3_Eventos_Delegados/Ejercicio5_Eventos/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Ejercicio5_Eventos;
+
+Console.WriteLine("Ejercicio #5\n");
+//Implementa una clase AlarmClock que tenga un evento OnAlarmRing.
+//El evento debe ser disparado cuando se alcance la hora configurada por el usuario.
+
+Console.Write("Ingresa la hora para la alarma (HH:mm:ss): ");
+string HoraDeAlarma = Console.ReadLine();
+
+DateTime TiempoDeAlarma;
+if (DateTime.TryParse(HoraDeAlarma, out TiempoDeAlarma))
+{
+    // esto es un poco extra, pero me gusto la funcionalidad =) : Si la hora ingresada ya pasó, se programa para el día siguiente
+    if (TiempoDeAlarma.TimeOfDay <= DateTime.Now.TimeOfDay)
+    {
+        TiempoDeAlarma = TiempoDeAlarma.AddDays(1);
+    }
+
+    AlarmClock alarma = new AlarmClock(TiempoDeAlarma);
+    alarma.EncendidoDeAlarma += AlarmClock_OnAlarmRing;
+
+    Console.WriteLine("Alarma configurada para: " + TiempoDeAlarma.ToString("HH:mm:ss"));
+    alarma.Inicio();
+
+    Console.WriteLine("Presiona Enter para salir...");
+    Console.ReadLine();
+
+    alarma.fin();
+}
+else
+{
+    Console.WriteLine("Hora inválida");
+}
+static void AlarmClock_OnAlarmRing(object sender, EventArgs e)
+{
+    Console.WriteLine("LEVANTATEEE!!!!!!!!");
+}
