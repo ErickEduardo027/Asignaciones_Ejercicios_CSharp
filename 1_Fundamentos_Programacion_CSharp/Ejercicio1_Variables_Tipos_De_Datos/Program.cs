dd51de5ba23e8f8305d36da4cc288dc74393c707
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-13 00:26:06 -0400

    Ejercicio #1 completado

--- 1_Fundamentos_Programacion_CSharp/Ejercicio1_Variables_Tipos_De_Datos/Program.cs
+++ 1_Fundamentos_Programacion_CSharp/Ejercicio1_Variables_Tipos_De_Datos/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-Console.WriteLine("hello world");
+using System.Xml.Serialization;
+Console.WriteLine("Ejercicio #1\n");
+//Define tres variables: una entera, una de punto flotante y una cadena de texto.
+//Inicializa las variables y muestra sus valores en la consola.
+
+int entero = 55;
+double flotante = 4.675434;
+string texto = "Manolo";
+
+Console.WriteLine(entero);
+Console.WriteLine(flotante);
+Console.WriteLine(texto);
