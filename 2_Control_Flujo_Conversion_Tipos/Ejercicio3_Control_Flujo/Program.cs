aa14b2f2f15a0fcc23afdcdce0b75aca59179b57
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-13 16:25:34 -0400

    Ejercicios #3 y #4 completados

--- 2_Control_Flujo_Conversion_Tipos/Ejercicio3_Control_Flujo/Program.cs
+++ 2_Control_Flujo_Conversion_Tipos/Ejercicio3_Control_Flujo/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #3\n");
+//Crea un programa que lea un número del usuario e imprima si es par o impar.
+//Utiliza una estructura if-else para decidir qué mensaje mostrar.
+Console.WriteLine("Ingresa un numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El numero es par");
+}
+else Console.WriteLine("El numero es impar");
--- 2_Control_Flujo_Conversion_Tipos/Ejercicio4_Conversion_Tipos/Program.cs
+++ 2_Control_Flujo_Conversion_Tipos/Ejercicio4_Conversion_Tipos/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #4\n");
+// Escribe un programa que convierta un número entero a un número decimal y luego lo convierta de nuevo a entero.
+// Explica cuándo puede ser útil realizar conversiones explícitas.
+Console.WriteLine("Ingresa un número entero: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"El número entero es: {numero}");
+
+double numeroDecimal = numero;
+Console.WriteLine("El número decimal es: " + numeroDecimal);
+
+int numeroConvertido = (int)numeroDecimal;
+Console.WriteLine("El número convertido de nuevo a entero es: " + numeroConvertido);
+
