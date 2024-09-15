681454b8abf5d05e3e0145430749ae96702abbac
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-14 21:05:43 -0400

    Ejercicio #9 y #10 completado

--- 5_Metodos_Extension/Ejercicio10_Aplicar_Metodo_Extension/Program.cs
+++ 5_Metodos_Extension/Ejercicio10_Aplicar_Metodo_Extension/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #10");
+//	Usa el método de extensión creado en el ejercicio anterior para contar las vocales en una oración introducida por el usuario.
+
+Console.WriteLine("Por favor, introduce una oración:");
+string oracion = Console.ReadLine();
+int cantidadVocales = oracion.ContarVocales();
+
+Console.WriteLine($"La oración introducida tiene {cantidadVocales} vocales.");
+
+public static class Extension
+{
+    public static int ContarVocales(this string texto)
+    {
+        int contador = 0;
+        char[] vocales = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        foreach (char letra in texto)
+        {
+            if (Array.Exists(vocales, v => v == letra))
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
--- 5_Metodos_Extension/Ejercicio9_Metodo_Extension/Program.cs
+++ 5_Metodos_Extension/Ejercicio9_Metodo_Extension/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #9");
+//Crea un método de extensión que extienda la clase string para contar cuántas veces aparece una vocal en un texto.
+
+string texto = "Hola me llamo Erick Eduardo y me gusta mucho la yaroa con mucho queso y carne.";
+int cantidadVocales = texto.ContarVocales();
+
+Console.WriteLine($"El texto contiene {cantidadVocales} vocales.");
+
+public static class Extension
+{
+    public static int ContarVocales(this string texto)
+    {
+        int contador = 0;
+        char[] vocales = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };//inclui las vocales mayusculas para contarlas (Si hay)
+
+        foreach (char letra in texto)
+        {
+            if (Array.Exists(vocales, v => v == letra))
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
