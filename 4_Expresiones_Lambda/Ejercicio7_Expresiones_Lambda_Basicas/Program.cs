5625fff0d2588be765467019651db888c684f3bd
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-14 20:44:26 -0400

    Ejercicio #7 y #8 completados

--- 4_Expresiones_Lambda/Ejercicio7_Expresiones_Lambda_Basicas/Program.cs
+++ 4_Expresiones_Lambda/Ejercicio7_Expresiones_Lambda_Basicas/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #7\n");
+/*Usa una expresión lambda para filtrar una lista de números y seleccionar solo los números pares.*/
+
+List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+
+List<int> numerosPares = numeros.FindAll(n => n % 2 == 0).ToList();
+
+Console.WriteLine("Números pares en la lista:");
+foreach (int num in numerosPares) Console.WriteLine(num);
--- 4_Expresiones_Lambda/Ejercicio8_Expresiones_Lambda_Complejas/Program.cs
+++ 4_Expresiones_Lambda/Ejercicio8_Expresiones_Lambda_Complejas/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #8");
+//Implementa una función que reciba una lista de estudiantes (nombre y edad) y utilice una expresión lambda para filtrar aquellos mayores de 18 años.
+
+List<Estudiante> Estudiantes = new List<Estudiante>
+{
+        new Estudiante { Nombre = "Juan", Edad = 17 },
+        new Estudiante { Nombre = "Ana", Edad = 19 },
+        new Estudiante { Nombre = "Carlos", Edad = 22 },
+        new Estudiante { Nombre = "Laura", Edad = 16 },
+        new Estudiante { Nombre = "Pedro", Edad = 21 }
+};
+
+List<Estudiante> mayoresDeEdad = Estudiantes.FindAll(e => e.Edad > 18).ToList();
+
+Console.WriteLine("Estudiantes mayores de 18 años: ");
+foreach (var item in mayoresDeEdad)
+{
+    Console.WriteLine($"{item.Nombre}, {item.Edad} años");
+}
+
+class Estudiante
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
