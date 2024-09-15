e544732f78c20376ab65f2648f97ff1a46d58bbd
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-15 13:01:46 -0400

    Ejercicio #14 y #15 completados

--- 8_Programacion_Orientada_Objetos/Ejercicio14_Herencia/Program.cs
+++ 8_Programacion_Orientada_Objetos/Ejercicio14_Herencia/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #14");
+//Crea una clase base Animal y dos clases derivadas Perro y Gato. 
+//Implementa un método Hablar en cada clase que imprima un sonido diferente para cada animal.
+
+Perro miPerro = new Perro();
+Console.WriteLine("El perro hace:");
+miPerro.Hablar();
+ 
+Gato miGato = new Gato();
+Console.WriteLine("El gato hace: ");
+miGato.Hablar();
+
+class Animal
+{
+    public virtual void Hablar()
+    {
+        Console.WriteLine("El animal hace un sonido.");
+    }
+}
+
+class Perro : Animal
+{
+    public override void Hablar()
+    {
+        Console.WriteLine("¡Guau, guau!");
+    }
+}
+class Gato : Animal
+{
+    public override void Hablar()
+    {
+        Console.WriteLine("¡Miau, miau!");
+    }
+}
--- 8_Programacion_Orientada_Objetos/Ejercicio15_Polimorfismo/Program.cs
+++ 8_Programacion_Orientada_Objetos/Ejercicio15_Polimorfismo/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Ejercicio #15");
+//Usa polimorfismo para permitir que tanto Perro como Gato sean tratados como Animal en una lista y que cada uno pueda "hablar" correctamente al recorrer la lista.
+
+List<Animal> animales = new List<Animal>()
+        {
+            new Perro { Nombre = "Firulais" },
+            new Gato { Nombre = "Michi" },
+            new Perro { Nombre = "Negro" },
+            new Gato { Nombre = "Pelusa" }
+        };
+
+foreach (var animal in animales)
+{
+    Console.Write($"{animal.Nombre}: ");
+    animal.Hablar();
+}
+
+class Animal
+{
+    public string Nombre { get; set; }
+
+    public virtual void Hablar()
+    {
+        Console.WriteLine("El animal hace un sonido.");
+    }
+}
+
+class Perro : Animal
+{
+    public override void Hablar()
+    {
+        Console.WriteLine("El perro dice: ¡Guau, guau!");
+    }
+}
+
+class Gato : Animal
+{
+    public override void Hablar()
+    {
+        Console.WriteLine("El gato dice: ¡Miau, miau!");
+    }
+}
+
