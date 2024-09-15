8abe1ca2909f4e09a09606054093f4f9e9506a6b
ErickEduardo027 <es2022-3938@uce.edu.do>
2024-09-15 13:42:21 -0400

    ejercicios #16 y #17 confirmados

--- 9_Genericos/Ejercicio16_Clase_Generica/Program.cs
+++ 9_Genericos/Ejercicio16_Clase_Generica/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #16");
+//Crea una clase genérica Caja<T> que pueda almacenar cualquier tipo de objeto. 
+//Implementa métodos para añadir y obtener objetos de la caja.
+
+Caja<int> cajaDeEnteros = new Caja<int>();
+cajaDeEnteros.Añadir(56);
+cajaDeEnteros.Añadir(72);
+Console.WriteLine($"La caja contiene {cajaDeEnteros.ContarObjetos()} objetos.");
+Console.WriteLine($"Se ha obtenido el objeto: {cajaDeEnteros.Obtener()}");
+Console.WriteLine($"La caja ahora contiene {cajaDeEnteros.ContarObjetos()} objetos.");
+
+Caja<string> cajaDeCadenas = new Caja<string>();
+cajaDeCadenas.Añadir("Erick");
+cajaDeCadenas.Añadir("Eduardo");
+Console.WriteLine($"La caja contiene {cajaDeCadenas.ContarObjetos()} objetos.");
+Console.WriteLine($"Se ha obtenido el objeto: {cajaDeCadenas.Obtener()}");
+Console.WriteLine($"La caja ahora contiene {cajaDeCadenas.ContarObjetos()} objetos.");
+
+class Caja<T>
+{
+    private List<T> objetos;  
+
+    public Caja()
+    {
+        objetos = new List<T>();
+    }
+
+    public void Añadir(T objeto)
+    {
+        objetos.Add(objeto);
+        Console.WriteLine($"{objeto} ha sido añadido a la caja.");
+    }
+
+    public T Obtener()
+    {
+        if (objetos.Count > 0)
+        {
+            T ultimoObjeto = objetos[objetos.Count - 1];
+            objetos.RemoveAt(objetos.Count - 1);  
+            return ultimoObjeto;
+        }
+        else
+        {
+            throw new InvalidOperationException("La caja está vacía.");
+        }
+    }
+    public int ContarObjetos()
+    {
+        return objetos.Count;
+    }
+}
--- 9_Genericos/Ejercicio17_Interfaz_Generica/Program.cs
+++ 9_Genericos/Ejercicio17_Interfaz_Generica/Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio #17");
+//Crea una interfaz genérica IContenedor<T> que defina métodos para agregar y obtener elementos. 
+//Implementa esta interfaz en la clase Caja<T>.
+
+IContenedor<int> cajaDeEnteros = new Caja<int>();
+cajaDeEnteros.Agregar(23984);
+cajaDeEnteros.Agregar(43257);
+Console.WriteLine($"La caja contiene {cajaDeEnteros.ContarElementos()} objetos.");
+Console.WriteLine($"Se ha obtenido el objeto: {cajaDeEnteros.Obtener()}");
+Console.WriteLine($"La caja ahora contiene {cajaDeEnteros.ContarElementos()} objetos.");
+
+IContenedor<string> cajaDeCadenas = new Caja<string>();
+cajaDeCadenas.Agregar("Erick Eduardo");
+cajaDeCadenas.Agregar("Santana Segura");
+Console.WriteLine($"La caja contiene {cajaDeCadenas.ContarElementos()} objetos.");
+Console.WriteLine($"Se ha obtenido el objeto: {cajaDeCadenas.Obtener()}");
+Console.WriteLine($"La caja ahora contiene {cajaDeCadenas.ContarElementos()} objetos.");
+
+public interface IContenedor<T>
+{
+    void Agregar(T item);     
+    T Obtener();              
+    int ContarElementos();     
+}
+
+public class Caja<T> : IContenedor<T>
+{
+    private List<T> objetos;  
+
+    public Caja()
+    {
+        objetos = new List<T>();
+    }
+    public void Agregar(T objeto)
+    {
+        objetos.Add(objeto);
+        Console.WriteLine($"{objeto} ha sido añadido a la caja.");
+    }
+    public T Obtener()
+    {
+        if (objetos.Count > 0)
+        {
+            T ultimoObjeto = objetos[objetos.Count - 1];
+            objetos.RemoveAt(objetos.Count - 1);  
+            return ultimoObjeto;
+        }
+        else
+        {
+            throw new InvalidOperationException("La caja está vacía.");
+        }
+    }
+    public int ContarElementos()
+    {
+        return objetos.Count;
+    }
+}
